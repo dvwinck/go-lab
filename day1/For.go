a4c6618a5532b242ff3c4b4e4bf0ad5e11135751
Diogo Winck <diogo.winck@gmail.com>
2021-04-28 13:07:08 -0300

    For aninhado

---
+++ day1/For.go
@@ -0,0 +1,50 @@
+// `for` is Go's only looping construct. Here are
+// some basic types of `for` loops.
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// The most basic type, with a single condition.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	// A classic initial/condition/after `for` loop.
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	// `for` without a condition will loop repeatedly
+	// until you `break` out of the loop or `return` from
+	// the enclosing function.
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	// You can also `continue` to the next iteration of
+	// the loop.
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+
+	for lin := 0; lin <= 5; lin++ {
+			
+		for col := 0; col <= 100; col++ {
+			if col %2 == 0 || lin == 0 || lin == 5 {
+				fmt.Printf("#")
+			}else{
+			    fmt.Printf(" ")
+			}
+		}
+		fmt.Println("")
+	}
+}
